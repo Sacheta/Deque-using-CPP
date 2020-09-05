b1f95d8554e10bbacb64265685bbd7871b3d87f8
Sacheta <63828173+Sacheta@users.noreply.github.com>
2020-09-05 18:03:36 +0530

    Update Deque_full_implementation.cpp

--- Deque_full_implementation.cpp
+++ Deque_full_implementation.cpp
@@ -6,18 +6,18 @@
 
 using namespace std;
 
-int arr[m],lleft=-1,rright=-1;
+int arr[m],lleft=-1,rright=-1; //declaring queue named arr and left and right pointers
 
-void sel(void);
-void rrightin(void);
-void lleftin(void);
-void rrightout(void);
-void lleftout(void);
-void display(void);
-bool isempty(void);
-bool isfull(void);
+void sel(void); //function to make selection of functions
+void rrightin(void); // function to enqueue an element from right side
+void lleftin(void); // function to enqueue an element from left
+void rrightout(void); // function to dequeue an element from right
+void lleftout(void); // function to dequeue an element from left
+void display(void); // function to display the queue
+bool isempty(void); // function to check whether the queue is empty
+bool isfull(void); // to check whether the queue is full
 
-void sel(void){
+void sel(void){ //definition of function to make selection
     int n;
     cin>>n;
     switch(n){
@@ -38,14 +38,14 @@ void sel(void){
     }
 }
 
-bool isempty(){
-    if(lleft==rright&&lleft==-1)
+bool isempty(){ 
+    if(lleft==rright&&lleft==-1) //condition of being empty
         return 1;
     return 0;
 }
 
 bool isfull(){
-    if(lleft==(rright+1)%m)
+    if(lleft==(rright+1)%m) //condition of queue being full
         return 1;
     return 0;
 }
@@ -59,9 +59,9 @@ void rrightin(void){
         return;
     }else if(isempty()){
         ++lleft;
-        arr[++rright]=a;
+        arr[++rright]=a; //incrementing both to enqueue in empty queue
     }else{
-        rright=(rright+1)%m;
+        rright=(rright+1)%m; //updating rright to make it go circular 
         arr[rright]=a;
     }
     display();
@@ -77,9 +77,9 @@ void lleftin(void){
         return;
     }else if(isempty()){
         ++rright;
-        arr[++lleft]=a;
+        arr[++lleft]=a;  //incrementing both to enqueue in empty queue
     }else{
-        lleft=(lleft-1)%m;
+        lleft=(lleft-1)%m; //updating lleft value to make it go circular
         arr[lleft]=a;
     }
     display();
@@ -90,13 +90,13 @@ void rrightout(void){
     if(isempty()){
         cout<<"Underflow!"<<endl;
         return;
-    }else if(lleft==rright&&lleft!=-1){
+    }else if(lleft==rright&&lleft!=-1){ //checking the condition of queue having just one element
         cout<<"Element: "<<arr[rright]<<" is deleted from right!"<<endl;
         rright=-1;
         lleft=-1;
     }else{
         cout<<"Element: "<<arr[rright]<<" is deleted from right!"<<endl;
-        rright=(rright-1)%m;
+        rright=(rright-1)%m; //updating rright value to delete from right and to make it go circular
     }
     display();
     sel();
@@ -106,7 +106,7 @@ void lleftout(void){
     if(isempty()){
         cout<<"Underflow!"<<endl;
         return;
-    }else if(lleft==rright&&lleft!=-1){
+    }else if(lleft==rright&&lleft!=-1){ //condition of having single element
         cout<<"Element: "<<arr[lleft]<<" is deleted from left!"<<endl;
         rright=-1;
         lleft=-1;
@@ -124,13 +124,13 @@ void display(){
         sel();
         return;
     }else{
-        int n=(m+rright-lleft)%m;
-        int i=lleft;
+        int n=(m+rright-lleft)%m; //setting n to total number of elements in the queue
+        int i=lleft; //setting i to lleft and queue goes from left to right
         cout<<"Queue is: "<<endl;
         cout<<"--------------------------------------"<<endl;
         while(n+1){
             cout<<arr[i]<<"\t";
-            i=(i+1)%m;
+            i=(i+1)%m; //updating value of i to make it go circular for n times
             --n;
         }
         cout<<"\n--------------------------------------"<<endl;
